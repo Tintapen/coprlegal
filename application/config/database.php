4599e513066bffa630f7052778a41b4b32e19b65
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2020-12-14 15:24:39 +0700

    Add database name and user

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'username' => 'corpora1_cla',
+	'password' => 'sqlKAMI2020',
+	'database' => 'corpora1_coprlegal',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
