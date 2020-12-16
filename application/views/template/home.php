2267bab3b5d1fb68aeb57c08815480d4f9e92a52
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2020-12-16 09:24:34 +0700

    Change h2 to h3 in class hero-content

--- application/views/template/home.php
+++ application/views/template/home.php
@@ -3,10 +3,10 @@
 		<div class="container">
 			<div class="row align-items-center">
 				<div class="hero-content">
-						<h2 class="mb-30 wow fadeInUp" data-wow-delay=".2s">Corporate Legal Academy</h2>
-						<p class="wow fadeInUp" data-wow-delay=".4s">Corporate Legal Academy (“CLA”) was found in 2020 by the Board of Management Atma Jaya Business Law Society (“ABLS”)2019/2020 in order to strengthen our contribution in the field of law academic by conducting a variety of events that have a strong quality yet influences law student to achieve their goals. </p>
-						<p>CLA is a series of webinar classes featuring alumni of Atma Jaya Faculty of Law across generation as speakers of this series. CLA aims to equip both Atma Jaya students and external students with the fundamental knowledge of business law along with its practical implementation. By joining CLA, you will get insights from the top speakers who are distinguished business law expertise.</p>
-						<p>This is a non-profit event, whereby any attendance fee from external will be donated as charity for people who are affected during the Covid-19.</p>
+					<h3 class="mb-30 wow fadeInUp" data-wow-delay=".2s">Corporate Legal Academy</h3>
+					<p class="wow fadeInUp" data-wow-delay=".4s">Corporate Legal Academy (“CLA”) was found in 2020 by the Board of Management Atma Jaya Business Law Society (“ABLS”)2019/2020 in order to strengthen our contribution in the field of law academic by conducting a variety of events that have a strong quality yet influences law student to achieve their goals. </p>
+					<p>CLA is a series of webinar classes featuring alumni of Atma Jaya Faculty of Law across generation as speakers of this series. CLA aims to equip both Atma Jaya students and external students with the fundamental knowledge of business law along with its practical implementation. By joining CLA, you will get insights from the top speakers who are distinguished business law expertise.</p>
+					<p>This is a non-profit event, whereby any attendance fee from external will be donated as charity for people who are affected during the Covid-19.</p>
 					<!--<div class="hero-btns">
 							<a href="#courses" class="main-btn wow fadeInUp" data-wow-delay=".6s">Courses</a>
 					</div>-->
@@ -19,5 +19,3 @@
 		</div>-->
 	</section>
 	<!--====== Home PART END ======-->
-
-	
