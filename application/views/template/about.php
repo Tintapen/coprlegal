e45d839e4f07e954f15d88ae659114be9deb026d
pomeepo <pomeepo@gmail.com>
2022-03-05 12:07:51 +0700

    Memperbaiki typo fiera

--- application/views/template/about.php
+++ application/views/template/about.php
@@ -19,13 +19,13 @@
 				</div>
 				<div class="col-xl-6 col-lg-6">
 				<div class="hero-content">
-				<h3 class="mb-30 wow fadeInUp" data-wow-delay=".2s">Fiera Salsabila N.
+				<h3 class="mb-30 wow fadeInUp" data-wow-delay=".2s">Feira Salsabila N.
 				
 				</h3>
 				<h4 class="mb-30 wow fadeInUp" data-wow-delay=".2s">Co-Founder of CLA</h4>
 				
-						<p class="wow fadeInUp" data-wow-delay=".4s">If i were to describe one word to describe CLA in one word, it'd be "Opportunity".<br> The classes conducted by CLA have provided every each of participants with a lot <br> of opportunities to learn or even more opportunities to work in the field of law.<br>
-						CLA were established to provide depth comprehensive knowledge related to legal <br> aspects of businesses and practical insights. Seeing all of the benefits CLA has offered,<br> it would be such a waste if you don't sign up for CLA 2022 !</p>
+						<p class="wow fadeInUp" data-wow-delay=".4s">If i were to describe one word to describe CLA in one word, it'd be "Opportunity".<br> The classes conducted by CLA have provided every each of participants with a lot of opportunities to learn or even more opportunities to work in the field of law.
+						CLA were established to provide depth comprehensive knowledge related to legal aspects of businesses and practical insights. Seeing all of the benefits CLA has offered, it would be such a waste if you don't sign up for CLA 2022 !</p>
 			
 					</div>
 				</div>
