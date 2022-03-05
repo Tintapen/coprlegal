5c88fe94429ad9b26cec53001143803642de79be
pomeepo <pomeepo@gmail.com>
2022-03-05 11:55:13 +0700

    Ubah Session

--- application/views/template/session.php
+++ application/views/template/session.php
@@ -7,49 +7,43 @@
 						<div class="row portfolio-description">
  				<div class="col-5 col-sm-3">
  					<div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
- 						<a class="nav-link active" id="vert-tabs-home-tab" data-toggle="pill" href="#vert-tabs-home" role="tab" aria-controls="vert-tabs-home" aria-selected="true">Day 1: Friday, 18 March 2022</a>
- 						<a class="nav-link" id="vert-tabs-profile-tab" data-toggle="pill" href="#vert-tabs-profile" role="tab" aria-controls="vert-tabs-profile" aria-selected="false">Day 2: Friday, 25 March 2022</a>
- 						<a class="nav-link" id="vert-tabs-messages-tab" data-toggle="pill" href="#vert-tabs-messages" role="tab" aria-controls="vert-tabs-messages" aria-selected="false">Day 3: Satuday, 26 March 2022</a>
- 						<a class="nav-link" id="vert-tabs-settings-tab" data-toggle="pill" href="#vert-tabs-settings" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 4: Friday, 1 April 2022</a>
- 						<a class="nav-link" id="vert-tabs-settings1-tab" data-toggle="pill" href="#vert-tabs-settings1" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 5: Saturday, 2 April 2022</a>
- 						<a class="nav-link" id="vert-tabs-settings2-tab" data-toggle="pill" href="#vert-tabs-settings2" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 6: Friday, 8 April 2022</a>
- 						<a class="nav-link" id="vert-tabs-settings3-tab" data-toggle="pill" href="#vert-tabs-settings3" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 7: Saturday, 9 April 2022</a>
- 						<a class="nav-link" id="vert-tabs-settings4-tab" data-toggle="pill" href="#vert-tabs-settings4" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 8: Saturday, 16 April 2022</a>
- 					</div>
+ 						<a class="nav-link active" id="vert-tabs-home-tab" data-toggle="pill" href="#vert-tabs-home" role="tab" aria-controls="vert-tabs-home" aria-selected="true">Day 1 : Friday, 25 March 2022</a>
+ 						<a class="nav-link" id="vert-tabs-profile-tab" data-toggle="pill" href="#vert-tabs-profile" role="tab" aria-controls="vert-tabs-profile" aria-selected="false">Day 2 : Saturday, 26 March 2022</a>
+ 						<a class="nav-link" id="vert-tabs-messages-tab" data-toggle="pill" href="#vert-tabs-messages" role="tab" aria-controls="vert-tabs-messages" aria-selected="false">Day 3 : Friday, 1 April 2022</a>
+ 						<a class="nav-link" id="vert-tabs-settings-tab" data-toggle="pill" href="#vert-tabs-settings" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 4 : Saturday, 2 April 2022</a>
+ 						<a class="nav-link" id="vert-tabs-settings1-tab" data-toggle="pill" href="#vert-tabs-settings1" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 5 : Friday, 8 April 2022</a>
+ 						<a class="nav-link" id="vert-tabs-settings2-tab" data-toggle="pill" href="#vert-tabs-settings2" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 6 : Saturday, 9 April 2022</a>
+ 						<a class="nav-link" id="vert-tabs-settings3-tab" data-toggle="pill" href="#vert-tabs-settings3" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 7 : Friday, 29 April 2022</a>
+ 						<a class="nav-link" id="vert-tabs-settings4-tab" data-toggle="pill" href="#vert-tabs-settings4" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 8 : Saturday, 30 April 2022</a>
+						 <a class="nav-link" id="vert-tabs-settings4-tab" data-toggle="pill" href="#vert-tabs-settings5" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 9 : Saturday, 7 May 2022</a>
+					</div>
  				</div>
  				<div class="col-7 col-sm-9">
  					<div class="tab-content" id="vert-tabs-tabContent">
  						<div class="tab-pane text-left fade show active" id="vert-tabs-home" role="tabpanel" aria-labelledby="vert-tabs-home-tab">
-						 <h5>Company Law</h5>
+						 <h5>Project Finance</h5>
 						 <br>
 						 <ul>
  								<li>
  									<b>10pm - 12pm Session</b>
  								</li>
-								 Legal Aspects in Corporate Restructuring
+								 The Perfection of Pledge, Fiduciary, and Mortgage and Its Scheme of Execution
  						</ul>
-						 <br>
-						 <ul>
-							 <li><b>10pm - 12pm Session</b></li>
-							 Legal and Business Aspects Related to Mergers and Acquisitions for Public Companies
-						 </ul>
  						</div>
  						<div class="tab-pane fade" id="vert-tabs-profile" role="tabpanel" aria-labelledby="vert-tabs-profile-tab">
  						<h5>Project Finance</h5><br>
 						 <ul>
- 								<li>
- 									<b>10pm - 12pm Session</b>
- 								</li>
- 								The Perfection of Pledge, Fiduciary, and Mortgage and Its Scheme of Execution
- 							</ul>
+							 <li><b>10pm - 12pm Session</b></li>
+							 Project  Financing  In  Indonesian Highway Construction
+						 </ul>
  						</div>
  						<div class="tab-pane fade" id="vert-tabs-messages" role="tabpanel" aria-labelledby="vert-tabs-messages-tab">
- 						<h5>Project Finance</h5>	<br>
+ 						<h5>Capital Market</h5>	<br>
 						 <ul>
  								<li>
  									<b>10pm - 12pm Session</b>
  								</li>
- 								Project Financing In Indonesian Highway Construction
+ 								Legal  Aspects   and   Impact   of   Stock Delisting
  							</ul>
  						</div>
  						<div class="tab-pane fade" id="vert-tabs-settings" role="tabpanel" aria-labelledby="vert-tabs-settings-tab">
@@ -58,16 +52,16 @@
  								<li>
  									<b>10pm - 12pm Session</b>
  								</li>
- 								Legal Aspects and Impact of Stock Delisting
+ 								The Legal Aspect of Global and National Bonds Offering
  							</ul>
  						</div>
  						<div class="tab-pane fade" id="vert-tabs-settings1" role="tabpanel" aria-labelledby="vert-tabs-settings1-tab">
- 						<h5>Capital Market</h5>	<br>
+ 						<h5>Mining & Resources</h5>	<br>
 						 <ul>
  								<li>
  									<b>10pm - 12pm Session</b>
  								</li>
- 								The Legal Aspect of Global and National Bonds Offering
+ 								Renewable  Resources   and  The  New Regime  of  Carbon  Policy  to  Improve Green Environment
  							</ul>
  							
  						</div>
@@ -77,31 +71,37 @@
  								<li>
  									<b>10pm - 12pm Session</b>
  								</li>
- 								Renewable Resources and The New Regime of Carbon Policy to Improve Green Environment
- 							</ul>
+								 Nickel  downstream  In  The  Electronic Vehicle Development Process
+							</ul>
  						</div>
  						<div class="tab-pane fade" id="vert-tabs-settings3" role="tabpanel" aria-labelledby="vert-tabs-settings3-tab">
- 						<h5>Mining & Resources</h5>	<br>
+ 						<h5>Company Law</h5>	<br>
 						 <ul>
  								<li>
  									<b>10pm - 12pm Session</b>
  								</li>
- 								Nickel downstream In The Electronic Vehicle Development Process
+ 								Legal Aspects in Corporate Restructuring
  							</ul>
  						</div>
  						<div class="tab-pane fade" id="vert-tabs-settings4" role="tabpanel" aria-labelledby="vert-tabs-settings4-tab">
- 						<h5>CV & Interview Workshop</h5>	<br>
+ 						<h5>Company Law</h5>	<br>
 						 <ul>
  								<li>
  									<b>10pm - 12pm Session</b>
  								</li>
- 								1.How To Make a Proper CV For Law Faculty Students<br>
-								2.Interview Preparation For Corporate Lawyer Careers
+ 								Legal  and  Business  Aspects  Related  to Mergers and Acquisitions for Public
  							</ul>
- 						
  						</div>
-
- 					</div>
+						 <div class="tab-pane fade" id="vert-tabs-settings5" role="tabpanel" aria-labelledby="vert-tabs-settings5-tab">
+						 <h5>CV & Interview Workshop</h5>	<br>
+						 <ul>
+ 								<li>
+ 									<b>10pm - 12pm Session</b>
+ 								</li>
+ 							1. How to Make a Proper CV for Law Faculity Student <br>
+							 2. Interview Preparation For Corporate Lawyers Careers
+							</ul>
+						</div>
  				</div>
  			</div>
 					</div>
