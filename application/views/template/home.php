a8977132a78876b8d5ef34124daf0c99c15d8b8a
Tintapen <okiper12@gmail.com>
2022-04-01 08:28:05 +0700

    Slideshow banner

--- application/views/template/home.php
+++ application/views/template/home.php
@@ -1,42 +1,42 @@
-	<!--====== Home PART START ======-->
-	<section id="home" class="hero-area bg_cover">
-		<!-- <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
-			<div class="carousel-indicators">
-				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+<!--====== Home PART START ======-->
+<section id="home" class="hero-area bg_cover">
+	<div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<!-- <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
+		</div>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="assets/images/banner_1.jpeg" class="w-100 d-block">
 			</div>
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img src="assets/images/blog/blog-1.jpg" class="w-100 d-block" alt="...">
-				</div>
-				<div class="carousel-item">
-					<img src="assets/images/blog/blog-2.jpg" class="w-100 d-block" alt="...">
-				</div>
-				<div class="carousel-item">
-					<img src="assets/images/blog/blog-3.jpg" class="w-100 d-block" alt="...">
-				</div>
+			<!-- <div class="carousel-item">
+				<img src="assets/images/blog/blog-2.jpg" class="w-100 d-block" alt="...">
 			</div>
-			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Previous</span>
-			</button>
-			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Next</span>
-			</button>
-		</div> -->
-		<div class="container">
-			<div class="row align-items-center">
-				<div class="hero-content">
-					<div class="hero-content" style="text-align: justify;">
-						<h3 class="mb-30 wow fadeInUp" data-wow-delay=".2s">Corporate Legal Academy</h3>
-						<p class="wow fadeInUp" data-wow-delay=".4s">Corporate Legal Academy (“CLA”) was found in 2020 by the Board of Management Atma Jaya Business Law Society (“ABLS”) 2019/2020 in order to strengthen our contribution in the field of law academic by conducting a variety of events that have a strong quality yet influences law student to achieve their goals. </p>
-						<p>CLA is a series of webinar classes featuring alumni of Atma Jaya Faculty of Law across generation as speakers of this series. CLA aims to equip both Atma Jaya students and external students with the fundamental knowledge of business law along with its practical implementation. By joining CLA, you will get insights from the top speakers who are distinguished business law expertise.</p>
-						<p>This is a non-profit event, whereby any attendance fee from external will be donated as charity for people who are affected during the Covid-19.</p>
-					</div>
+			<div class="carousel-item">
+				<img src="assets/images/blog/blog-3.jpg" class="w-100 d-block" alt="...">
+			</div> -->
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+	<!-- <div class="container">
+		<div class="row align-items-center">
+			<div class="hero-content">
+				<div class="hero-content" style="text-align: justify;">
+					<h3 class="mb-30 wow fadeInUp" data-wow-delay=".2s">Corporate Legal Academy</h3>
+					<p class="wow fadeInUp" data-wow-delay=".4s">Corporate Legal Academy (“CLA”) was found in 2020 by the Board of Management Atma Jaya Business Law Society (“ABLS”) 2019/2020 in order to strengthen our contribution in the field of law academic by conducting a variety of events that have a strong quality yet influences law student to achieve their goals. </p>
+					<p>CLA is a series of webinar classes featuring alumni of Atma Jaya Faculty of Law across generation as speakers of this series. CLA aims to equip both Atma Jaya students and external students with the fundamental knowledge of business law along with its practical implementation. By joining CLA, you will get insights from the top speakers who are distinguished business law expertise.</p>
+					<p>This is a non-profit event, whereby any attendance fee from external will be donated as charity for people who are affected during the Covid-19.</p>
 				</div>
 			</div>
 		</div>
-	</section>
-	<!--====== Home PART END ======-->
+	</div> -->
+</section>
+<!--====== Home PART END ======-->
